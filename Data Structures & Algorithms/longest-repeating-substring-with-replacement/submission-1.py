bb4e9a9329d19522888e9a9a1be30f1f401eae4f
Hashir Ahmed <59901350+HashirA123@users.noreply.github.com>
2026-04-05 18:56:39 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # sliding window again, but need to keep track of
+        # most popular element, so we know which others
+        # to count as replaced
+
+        max_size = 0
+        max_val = 0
+
+        l, r = 0, 0
+
+        store = defaultdict(int)
+
+        while r < len(s):
+            store[s[r]] += 1
+
+            max_val = max(max_val, store[s[r]])
+            while ((r-l+1) - max_val) > k:
+                store[s[l]] -= 1
+                l += 1
+
+            max_size = max(max_size, r-l+1)
+            r += 1
+        
+        return max_size
